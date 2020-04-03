f9929fc330afffaa948790848d257a95eb078682
tratitude <tratitude@gmail.com>
2020-04-03 15:22:32 +0200

    add 3d option

--- runHandTrackingGPU.sh
+++ runHandTrackingGPU.sh
@@ -1,2 +1,3 @@
-GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/multi_hand_tracking/multi_hand_tracking_gpu \
+GLOG_logtostderr=1 
+bazel-bin/mediapipe/examples/desktop/multi_hand_tracking/multi_hand_tracking_gpu \
     --calculator_graph_config_file=mediapipe/graphs/hand_tracking/multi_hand_tracking_mobile.pbtxt
