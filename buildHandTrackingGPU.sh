487753e1b5ab49f5d58d8560a91250920622d43b
Kellen Wu <tratitude@gmail.com>
2020-04-13 16:15:41 +0200

    Update buildHandTrackingGPU.sh
    build with GAME_MODE and without IMSHOW_ENABLE

--- buildHandTrackingGPU.sh
+++ buildHandTrackingGPU.sh
@@ -1,5 +1,5 @@
 bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS \
-    --copt="-DGAME_MODE" --copt="-DIMSHOW_ENABLE" \
+    --copt="-DGAME_MODE" \
     mediapipe/examples/desktop/multi_hand_tracking:multi_hand_tracking_gpu
 
 # game mode
