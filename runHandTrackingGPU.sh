3c71d05bf953905991c12305a05a35bad470d97a
tratitude <tratitude@gmail.com>
2020-04-18 21:34:28 +0200

    add interpreter command

--- runHandTrackingGPU.sh
+++ runHandTrackingGPU.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 GLOG_logtostderr=1 
-bazel-bin/mediapipe/examples/desktop/multi_hand_tracking/multi_hand_tracking_gpu \
+exec bazel-bin/mediapipe/examples/desktop/multi_hand_tracking/multi_hand_tracking_gpu \
     --calculator_graph_config_file=mediapipe/graphs/hand_tracking/multi_hand_tracking_mobile.pbtxt
