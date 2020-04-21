4574c3c5125dc94c797be5320067c4c03eb75266
Kellen Wu <tratitude@gmail.com>
2020-04-21 14:33:28 +0200

    Update buildHandTrackingGPU.sh

--- buildHandTrackingGPU.sh
+++ buildHandTrackingGPU.sh
@@ -1,12 +1,14 @@
-bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS \
+bazel build -c opt --copt -DMESA_EGL_NO_X11_HEADERS --copt -DEGL_NO_X11 \
     --copt="-DGAME_MODE" \
     mediapipe/examples/desktop/multi_hand_tracking:multi_hand_tracking_gpu
 
-# game mode
+# game mode, need created shm in receiver before running handtracking
 #--copt="-DGAME_MODE" \
-# performace mode
-#--cxxopt -DPER_MODE \
-# define mode
-#--cxxopt -DDEFINE_MODE \
+# performace mode, create shm in handtracking
+#--copt -DPER_MODE \
+# define mode, create shm in handtracking
+#--copt -DDEFINE_MODE \
 # image show
 #--copt="-DIMSHOW_ENABLE" \
+# condition lock on shm
+#--copt="-DCOND_LOCK_ENABLE \
