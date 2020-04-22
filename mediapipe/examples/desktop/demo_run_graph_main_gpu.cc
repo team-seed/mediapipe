d3f8435763a3e51a6b90d103acc212fe0944f639
tratitude <tratitude@gmail.com>
2020-04-23 01:55:57 +0200

    add camId config

--- mediapipe/examples/desktop/demo_run_graph_main_gpu.cc
+++ mediapipe/examples/desktop/demo_run_graph_main_gpu.cc
@@ -84,7 +84,7 @@ DEFINE_string(output_video_path, "",
   if (load_video) {
     capture.open(FLAGS_input_video_path);
   } else {
-    capture.open(0);
+    capture.open(hg.camID);
   }
   RET_CHECK(capture.isOpened());
 
